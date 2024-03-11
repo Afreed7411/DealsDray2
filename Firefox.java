54398f315d7fad0d1e793e74b3805ee8f5c6dfa7
Afreed7411 <135531217+Afreed7411@users.noreply.github.com>
2024-03-11 18:24:03 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,18 @@
+package sel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Day1 {
+    public static void main(String[] args) {
+     WebDriver driver = new FirefoxDriver();
+     driver.manage().window().maximize();
+
+    
+     driver.get("https://www.getcalley.com/page-sitemap.xml");
+
+    }
+}
